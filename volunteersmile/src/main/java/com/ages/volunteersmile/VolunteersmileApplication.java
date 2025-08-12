a0d4ffa2d6b8db9af572162d9cc488c31ba2600d
Angelo Corte <angelo.corte@edu.pucrs.br>
2025-08-12 18:45:09 -0300

    first commit arch

---
+++ volunteersmile/src/main/java/com/ages/volunteersmile/VolunteersmileApplication.java
@@ -0,0 +1,13 @@
+package com.ages.volunteersmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteersmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteersmileApplication.class, args);
+	}
+
+}
---
+++ volunteersmile/src/test/java/com/ages/volunteersmile/VolunteersmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ages.volunteersmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteersmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
