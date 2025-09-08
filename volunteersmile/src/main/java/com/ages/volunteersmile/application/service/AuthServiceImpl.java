37e512ee1e6b47e9b02b87f4293be90acf6609bf
Nicholas Spolti <nicholasspoltidesouza@gmail.com>
2025-09-07 23:46:14 -0300

    fix: auth for admin and volunteer

--- volunteersmile/src/main/java/com/ages/volunteersmile/application/service/AuthServiceImpl.java
+++ volunteersmile/src/main/java/com/ages/volunteersmile/application/service/AuthServiceImpl.java
@@ -2,25 +2,25 @@ package com.ages.volunteersmile.application.service;
 
 import java.util.Optional;
 
+import com.ages.volunteersmile.application.dto.UserDTO;
+import com.ages.volunteersmile.application.mapper.UserDataMapper;
+import com.ages.volunteersmile.domain.global.model.User;
+import com.ages.volunteersmile.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ages.volunteersmile.application.dto.LoginRequestDTO;
 import com.ages.volunteersmile.application.dto.LoginResponseDTO;
-import com.ages.volunteersmile.application.dto.VolunteerDTO;
-import com.ages.volunteersmile.application.mapper.VolunteerDataMapper;
-import com.ages.volunteersmile.domain.volunteer.model.Volunteer;
-import com.ages.volunteersmile.repository.VolunteerRepository;
 import com.ages.volunteersmile.utils.JwtUtil;
 
 @Service
 public class AuthServiceImpl implements AuthService {
     @Autowired
-    private VolunteerRepository volunteerRepository;
+    private UserRepository userRepository;
     
     @Autowired
-    private VolunteerDataMapper volunteerDataMapper;
+    private UserDataMapper userDataMapper;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -30,17 +30,15 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public LoginResponseDTO login(LoginRequestDTO request) {
-        Optional<Volunteer> volunteerOpt = volunteerRepository.findByEmail(request.getEmail());
-        if (volunteerOpt.isEmpty() || !passwordEncoder.matches(request.getPassword(), volunteerOpt.get().getPassword())) {
+            Optional<User> userOpt = userRepository.findByEmail(request.getEmail());
+        if (userOpt.isEmpty() || !passwordEncoder.matches(request.getPassword(), userOpt.get().getPassword())) {
             throw new RuntimeException("Usuário ou senha inválidos");
         }
         
-        VolunteerDTO volunteerDTO = volunteerDataMapper.mapFrom(volunteerOpt.get());
-        String token = jwtUtil.generateToken(volunteerDTO);
+        UserDTO userDTO = userDataMapper.mapFrom(userOpt.get());
+        String token = jwtUtil.generateToken(userDTO);
         return new LoginResponseDTO(
-            token,
-            volunteerDTO.getName(),
-            volunteerDTO.getEmail()
+            token, userDTO.getName(), userDTO.getEmail()
         );
     }
 }
--- volunteersmile/src/main/java/com/ages/volunteersmile/utils/JwtUtil.java
+++ volunteersmile/src/main/java/com/ages/volunteersmile/utils/JwtUtil.java
@@ -3,10 +3,9 @@ package com.ages.volunteersmile.utils;
 import java.security.Key;
 import java.util.Date;
 
+import com.ages.volunteersmile.application.dto.UserDTO;
+import com.ages.volunteersmile.domain.global.model.User;
 import org.springframework.stereotype.Component;
-
-import com.ages.volunteersmile.application.dto.VolunteerDTO;
-import com.ages.volunteersmile.domain.volunteer.model.Volunteer;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -19,18 +18,18 @@ public class JwtUtil {
     private final long EXPIRATION = 1000 * 60 * 60 * 24;
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-    public String generateToken(Volunteer volunteer) {
+    public String generateToken(User user) {
         return Jwts.builder()
-                .setSubject(volunteer.getEmail())
+                .setSubject(user.getEmail())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String generateToken(VolunteerDTO volunteerDTO) {
+    public String generateToken(UserDTO userDTO) {
         return Jwts.builder()
-                .setSubject(volunteerDTO.getEmail())
+                .setSubject(userDTO.getEmail())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
                 .signWith(key, SignatureAlgorithm.HS256)
