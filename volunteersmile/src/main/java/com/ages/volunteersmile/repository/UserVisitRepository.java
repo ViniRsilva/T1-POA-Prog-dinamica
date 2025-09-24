34053ea1c36b805532ea494f3425265e3800a679
Henrique Gama Bueno <henrique.bueno@edu.pucrs.br>
2025-09-24 00:33:21 +0000

    fix(repository): importando java.util.Optional; e java.time.LocalDateTime;

--- volunteersmile/src/main/java/com/ages/volunteersmile/repository/UserVisitRepository.java
+++ volunteersmile/src/main/java/com/ages/volunteersmile/repository/UserVisitRepository.java
@@ -2,6 +2,8 @@ package com.ages.volunteersmile.repository;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.Optional;
+import java.time.LocalDateTime;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
